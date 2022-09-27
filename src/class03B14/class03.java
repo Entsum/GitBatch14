57e289b30a9ef020a91d387d4967faf422964a69
TarasH <Taras.hulyar@gmail.com>
2022-09-27 19:50:23 -0400

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -5,5 +5,6 @@ public class class03 {
     public static void main(String[] args) {
 
         System.out.println("hey this is a git class");
+        System.out.println("hey this is change in my project");
     }
 }
